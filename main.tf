ddd670f809c41e72dde3ebee5b436630c1c9543d
David <seuemail@exemplo.com>
2025-10-18 18:15:03 -0300

    Fix para subir máquina para github

--- main.tf
+++ main.tf
@@ -1,5 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"  # Compatível com a maioria dos recursos básicos
+    }
+  }
+
+  required_version = ">= 1.5"
+}
+
 provider "aws" {
   region = "us-east-1"
+}
+
+# ---------- Data source: Default VPC ----------
+data "aws_vpc" "default" {
+  default = true
+}
+
+# ---------- Data source: Subnet pública da AZ ----------
+data "aws_subnet" "default" {
+  default_for_az      = true
+  availability_zone   = "us-east-1a"
+  vpc_id              = data.aws_vpc.default.id
 }
 
 # ---------- Security Group ----------
@@ -33,9 +56,9 @@ resource "aws_security_group" "ec2_sg" {
 resource "aws_instance" "web" {
   ami                         = "ami-0c02fb55956c7d316" # Amazon Linux 2
   instance_type               = "t2.micro"
+  subnet_id                   = data.aws_subnet.default.id
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
-  subnet_id                   = data.aws_subnet_ids.default.ids[0]
 
   tags = {
     Name = "TerraformWebServer"
@@ -49,13 +72,4 @@ resource "aws_instance" "web" {
               sudo systemctl enable httpd
               echo "<h1>Hello from Terraform EC2</h1>" > /var/www/html/index.html
               EOF
-}
-
-# ---------- Data sources for default VPC ----------
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
 }
